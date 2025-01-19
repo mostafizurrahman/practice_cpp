0e0565e9826bb1b305486d1156a38f7e3092c254
mostafizur-mony <rahman.mostafizur@bkash.com>
2025-01-19 09:30:52 +0600

    uncommented

--- coding.cpp
+++ coding.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream> 
+#include <map>
 using namespace std;
 
 
@@ -23,25 +24,37 @@ vector<int> _arr_target_sum() {
     cin>>target;    
 
     int firstIndex, lastInde;
-    
+    unordered_map<int, int> num_map;
+    // for(int i = 0; i < arr.size(); i++) {
+    //     int check = arr.at(i);
+    //     for(int j = i + 1; j < arr.size(); j++ ){
+    //         if (check + arr[j] == target) {
+    //             indices.push_back(i);
+    //             indices.push_back(j);
+    //         }
+    //     }
+    // }
+
+
     for(int i = 0; i < arr.size(); i++) {
-        int check = arr.at(i);
-        for(int j = i + 1; j < arr.size(); j++ ){
-            if (check + arr[j] == target) {
-                indices.push_back(i);
-                indices.push_back(j);
-            }
+        int complement = target - arr.at(i);
+        if (num_map.find(complement) != num_map.end()) {
+            indices.push_back(num_map[complement]);
+            indices.push_back(i);
         }
+        num_map[arr[i]] = i;
     }
+
     return indices;
 
+
 }
 
-int main() {
-    vector<int> indices = _arr_target_sum();
-    int i = 0;
-    for(; i < indices.size(); i++) {
-        cout << "indices " << indices.at(i) << " & " << indices.at(++i)<< " \n" ; 
-    }
-    return 0;
-}
+ int main() {
+     vector<int> indices = _arr_target_sum();
+     int i = 0;
+     for(; i < indices.size(); i++) {
+         cout << "indices " << indices.at(i) << " & " << indices.at(++i)<< " \n" ;
+     }
+     return 0;
+ }
